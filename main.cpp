26486363a626414bae99a9f1cf431693b38e2be1
cramr <93094049+kraemr@users.noreply.github.com>
2023-10-15 15:46:55 +0200

    added comments, audio plays in own thread, TODO: GUI

--- main.cpp
+++ main.cpp
@@ -6,8 +6,14 @@
 #include "DFT.hpp"
 #include <iostream>
 #include <cmath> // implement round() later to avoid deps
+#include <thread>
+
+#define GLSL_AUDIO_START 0
+#define GLSL_AUDIO_STOP 1
+#define GLSL_AUDIO_NEXT_SONG 2
+#define GLSL_AUDIO_PREV_SONG 3
 #define BITS 8
-#define DEBUG
+//#define DEBUG
 
 
 /*TODO:
@@ -29,17 +35,19 @@ Basically we calc the number of frames per second (frames * bytes_per_frame)
 first second (8 bit mono) --> start 0  end 24 * 1152
 second second --> start 24*1152  end  24 * 1152 * 2
 
+Ok so the simplest way to visualize audio would be to just run a second program/thread, that reads from the alsa device and displays the currently played audio
+This has the side effect of also visualizing audio, that does not come from the music player ..., but that should be fine for now.
+
 
 https://www.mpg123.de/api/group__mpg123__input.shtml#ga072669ae9bde29eea8cffa4be10c7345
 https://mpg123.de/api/group__mpg123__seek.shtml // seeking pos i.e frames
 Instead of loading all mp3 data to memory its possible to just skip around in the mp123 stream with mpg123_seek.
 However i would have to put ao_play in its own thread to be able to pause, skip ...
-
 */
+
 bool musicplayed= false;
 bool g_samplebuffer_alloced = false;
 mpg123_handle * mh;
-unsigned char * g_samplebuffer; // The buffer that contains the PCM samples
 size_t done;
 int err;
 int driver;
@@ -53,8 +61,9 @@ unsigned int bytesperframe=0;
 double fps=0; // MPEG3Frames per second
 double rawfps=0; // UNROUNDED frames per second
 long long unsigned int bytesread = 0;
+bool paused=false;
 
-
+#define FRAMES_PLAY_PER_READ 4 // This would create a delay when pressing stop,play ..., but lessens the load on cpu
 // for now this uses raylib
 // Also this will handle all the ui
 void init_shader_vis(int width,int height){
@@ -63,18 +72,34 @@ void init_shader_vis(int width,int height){
 }
 
 
+void resume_play_at_sec(unsigned int sec){
+	#ifdef DEBUG
+		std::cout << "Buffer Size for Playing Audio: " << bytesperframe * FRAMES_PLAY_PER_READ << std::endl;
+	#endif
+	unsigned char buf[bytesperframe * FRAMES_PLAY_PER_READ]={0};
+        mpg123_seek_frame(mh,unsigned( fps * sec ),SEEK_SET); // Its not 100% accurate a second here is like 1008 ms instead of 1000 but thats fine ... i think
+	unsigned int mpg123_ret = MPG123_OK;
+	long unsigned int done = 0;
+	long unsigned int framesplayed = 0;
+	long unsigned int current_sec = 0;
+	while (paused == false && mpg123_ret == MPG123_OK){
+		mpg123_ret = mpg123_read(mh, buf ,bytesperframe * FRAMES_PLAY_PER_READ, &done);
+		ao_play(dev,(char*)buf,bytesperframe * FRAMES_PLAY_PER_READ);
+		framesplayed += FRAMES_PLAY_PER_READ;
+		if (framesplayed / fps >= 1) {
+			current_sec +=1;
+			framesplayed = 0;
+		}
+		#ifdef DEBUG
+			std::cout << "current Second: "<< current_sec << std::endl;
+		#endif
+	}
+}
 
 void mpg_print_fmt(){
 	std::cout << "bits: "<< format.bits << " rate:"<<format.rate << " channels:"<<format.channels <<  " ,byte_fmt:"<<format.byte_format << std::endl;
 }
 
-// This fills the sample buffer with n seconds of samples
-long long unsigned int fill_buffer(){
-
-}
-
-
-// gets the frame duration
 //https://stackoverflow.com/questions/6220660/calculating-the-length-of-mp3-frames-in-milliseconds
 double get_frame_duration_ms_MP3(unsigned int sampleratehz){
 	return 1152.0 / double(sampleratehz) * 1000.0; // This should be correct ?!
@@ -89,58 +114,29 @@ void initPlay(char * abspath){
 	mpg123_init();
 	mh = mpg123_new(NULL, &err);
 	buffer_size = mpg123_outblock(mh);
-	if (g_samplebuffer_alloced){
-		free(g_samplebuffer); // later on check if buffer  is big enough for new file instead of always freeing
-		g_samplebuffer_alloced = true;
-	}
 	mpg123_open(mh, abspath);
 	mpg123_getformat(mh, &rate, &channels, &encoding);
 	format.bits = mpg123_encsize(encoding) * BITS;
-    format.rate = rate;
-    format.channels = channels;
-    format.byte_format = AO_FMT_NATIVE;
-    format.matrix = 0;
-    bytesperframe = (format.bits/8 * channels * 1152);
-    rawfps =  1000.0 / double(get_frame_duration_ms_MP3(format.rate));
-   fps = ceil(rawfps);
-    #ifdef DEBUG
-    std::cout << "Error: " << err << std::endl;
+        format.rate = rate;
+        format.channels = channels;
+        format.byte_format = AO_FMT_NATIVE;
+        format.matrix = 0;
+        bytesperframe = (format.bits/8 * channels * 1152);
+    	rawfps =  1000.0 / double(get_frame_duration_ms_MP3(format.rate));
+        fps = ceil(rawfps);
+        #ifdef DEBUG
+	std::cout << "Error: " << err << std::endl;
 	std::cout << "Buffer Size: " << buffer_size << std::endl;
 	std::cout << "Frames Per Buffer Read: " << buffer_size/4608 << std::endl;
 	std::cout << "Milliseconds Per Buffer Read: " << get_frame_duration_ms_MP3(format.rate) * (buffer_size/4608) << std::endl;
 	std::cout << "ms per frame: " << get_frame_duration_ms_MP3(format.rate) << std::endl;
         std::cout << "bytes per frame: " << bytesperframe << std::endl;
-	std::cout << "frames per second" << fps <<std::endl;
-
- 	std::cout << "bytes per second" << (format.bits/8 * channels * 1152) * (1000.0 / double(get_frame_duration_ms_MP3(format.rate))) << std::endl;
+	std::cout << "frames per second: " << fps <<std::endl;
+ 	std::cout << "bytes per second: " << (format.bits/8 * channels * 1152) * (1000.0 / double(get_frame_duration_ms_MP3(format.rate))) << std::endl;
 	#endif
-        g_samplebuffer = (unsigned char*) malloc(100 * 1000 * 1000 * sizeof(unsigned char));
 }
-
-
-void start_play_At(unsigned int second){
-}
-
-void readSecond(){
-    mpg123_read(mh, g_samplebuffer, fps * bytesperframe, &done);
-}
-
-void readFileIntoBuffer(){
-	int ret=0;
-	long unsigned int done;
-	bytesread=0;
-	while (ret == MPG123_OK){
-		ret = mpg123_read(mh, g_samplebuffer,bytesperframe, &done);
-		g_samplebuffer += bytesperframe; // Move the pointer so the buffer gets filled
-		bytesread += bytesperframe;
-	}
-	g_samplebuffer -= bytesread; // reset the pointer to 0 index
-}
-
 
 void cleanup(){
-    CloseWindow();        // Close window and OpenGL context
-    free(g_samplebuffer);
     ao_close(dev);
     mpg123_close(mh);
     mpg123_delete(mh);
@@ -150,16 +146,29 @@ void cleanup(){
 
 int main(int argc, char *argv[])
 {
-    SetTraceLogLevel(LOG_ERROR);
+    SetTraceLogLevel(LOG_ERROR); // Disable annoying raylib error messages
     if(argc < 2) exit(0);
     ao_initialize();
     driver = ao_default_driver_id();
     initPlay(argv[1]);
     dev = ao_open_live(driver, &format, NULL);
     mpg_print_fmt();
-    init_shader_vis(1900,1024);
-    readFileIntoBuffer();
+    bool exit = false;
+    unsigned int sec = 0;
+    unsigned int cmd = 0;
+    paused = true;
+    std::thread t1;
 
+    while(!exit){
+	std::cin >> cmd;
+	if(cmd == 0){
+		paused = false;
+		t1 = std::thread(resume_play_at_sec,0);
+	}else if(cmd == 1){
+		paused = true;
+		t1.join();
+	}
+    }
 	   // std::vector<double> spectrum = traceSpectrum(g_samplebuffer,bytespf,1024,0);
 /*	    BeginDrawing();
             ClearBackground(RAYWHITE);
@@ -170,20 +179,6 @@ int main(int argc, char *argv[])
 	    }
             EndDrawing();*/
 //	    threaded_ao_play();
-    
-
-//    unsigned int fpsbytes = fps * bytesperframe;
-    unsigned int second=0;
-    while( offset < bytesread - rawfps * bytesperframe){
-	//std::cin >> second;
-	offset = second * fps * bytesperframe;
-	ao_play(dev, (char*)g_samplebuffer+offset, rawfps * bytesperframe); // put this in its own thread, so that music and animation dont fight for cpu ressources
-	second++;
-  }
-    #ifdef DEBUG
-//	std::cout << fpsbytes <<std::endl; 
-    #endif
-// }
    cleanup();
    return 0;
 }
