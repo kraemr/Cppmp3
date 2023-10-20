68dbd502ee65da9fab29306739345eb33aedc9fb
Robin Krämer <93094049+kraemr@users.noreply.github.com>
2023-10-21 01:31:47 +0200

    Delete ThreadedAudioPlayer.cpp

--- ThreadedAudioPlayer.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#include <thread>
-
-void playBufferAudio(unsigned char * buffer, unsigned int buffer_size){
-	std::cout << "Threaded playBufferAudio size:" << buffer_size << std::endl;
-}
