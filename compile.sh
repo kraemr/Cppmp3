f7908c1ef55056fd07dc8c7ab29fd4888ed12d5e
cramr <93094049+kraemr@users.noreply.github.com>
2023-10-25 13:02:48 +0200

    Cleanup/Refactor

--- compile.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-g++ main.cpp playlist.cpp stdafx.cpp  DFT.cpp -lmpg123 -lao include/libraylib.a -fsanitize=address -Wall -lfftw3
--- include/DFT.hpp
+++
@@ -1,2 +0,0 @@
-std::vector<double> traceSpectrum(unsigned char* buffer,unsigned int n, unsigned int points,unsigned int offset);
-void fft(std::vector<double>& real, std::vector<double>& imag,bool inverse);
